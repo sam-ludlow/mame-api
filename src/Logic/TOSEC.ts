fbb83e8a46d61d2b8453f2f99c874e3eb05e7376
Sam @ Spludlow <sam@spludlow.co.uk>
2021-12-02 18:35:38 +0000

    dev

--- src/Logic/TOSEC.ts
+++ src/Logic/TOSEC.ts
@@ -19,6 +19,18 @@ export const BuildCache = async (): Promise<any> => {
         await sqlClient.Close();
     }
 
+    const games: any[] = cache.game;
+    const roms: any[] = cache.rom;
+
+    const gameIdDatafileIdLookup: any = {};
+    games.forEach((game: any) => {
+        gameIdDatafileIdLookup[game.game_Id] = game.datafile_Id;
+    });
+
+    roms.forEach((rom: any) => {
+        rom.datafile_Id = gameIdDatafileIdLookup[rom.game_Id];
+    });
+
     return {
         Tables: cache,
     };
@@ -29,6 +41,9 @@ export class TOSECApplicationServer implements Model.ApplicationServer {
     public Cache: any;
     public Key: string;
 
+    public DatafileIdNameLookup: any = {};
+    public GameIdNameLookup: any = {};
+
     constructor(key: string, cache: any) {
 
         this.Key = key;
@@ -37,21 +52,128 @@ export class TOSECApplicationServer implements Model.ApplicationServer {
         //  Non-persistant caches
         this.Cache['Games'] = {};
 
+        const datafiles: any[] = cache.Tables.datafile;
+        datafiles.forEach((datafile) => {
+            this.DatafileIdNameLookup[datafile.datafile_Id] = datafile.name;
+        });
+
+        const roms: any[] = cache.Tables.rom;
+        roms.forEach((rom) => {
+            this.GameIdNameLookup[rom.rom_Id] = rom.name;
+        });
     }
 
+    private Filter = (items: any[], properptyNames: any, queryParameters: any): any => {
+        const filter: any = {};
 
-    public GetDataFiles = async (context: Tools.Context): Promise<any> => {
+        properptyNames.forEach((properptyName: string) => {
+            const filterValues: string[] = queryParameters[`filter_${properptyName}`];
+            if (filterValues !== undefined) {
+                const ids: number[] = this.Cache.Tables[properptyName]
+                    .filter((item: any) => filterValues.includes(item.name))
+                    .map((item: any) => item[`${properptyName}_Id`]);
 
-        const offset: number = context.request.queryParameters['offset'].slice(-1)[0];
-        const limit: number = context.request.queryParameters['limit'].slice(-1)[0];
+                items = items.filter((rom: any) => ids.includes(rom[`${properptyName}_Id`]));
 
-        const datafiles = this.Cache.Tables['datafile'];
+                filter[properptyName] = filterValues;
+            }
+        });
 
         return {
-            count: datafiles.length,
-            results: datafiles.slice(offset, offset + limit),
+            items,
+            filter,
         };
     }
 
+    public GetDataFiles = async (context: Tools.Context): Promise<any> => {
+
+        let datafiles = this.Cache.Tables['datafile'];
+
+        const count = datafiles.length;
+
+        // Slice
+        const offset: number = context.request.queryParameters['offset'].slice(-1)[0];
+        const limit: number = context.request.queryParameters['limit'].slice(-1)[0];
+        datafiles = datafiles.slice(offset, offset + limit);
+
+        // Prepare
+        datafiles = datafiles.map((item: any) => ({...item}));
+        datafiles.forEach((datafile: any) => {
+            delete datafile.datafile_Id;
+        });
+
+        return {
+            count: count,
+            results: datafiles,
+        };
+    }
+
+    public GetGames = async (context: Tools.Context): Promise<any> => {
+
+        const facetPropertyNames = ['datafile'];
+
+        let games: any[] = this.Cache.Tables['game'];
+
+        // Filter
+        const { items, filter } = this.Filter(games, facetPropertyNames, context.request.queryParameters);
+        games = items;
+
+        // Slice
+        const count = games.length;
+        const offset: number = context.request.queryParameters['offset'].slice(-1)[0];
+        const limit: number = context.request.queryParameters['limit'].slice(-1)[0];
+        games = games.slice(offset, offset + limit);
+
+        // Prepare
+        games = games.map((item: any) => ({...item}));
+        games.forEach((game: any) => {
+
+            game.datafile_name = this.DatafileIdNameLookup[game.datafile_Id];
+
+            delete game.game_Id;
+            delete game.datafile_Id;
+        });
+        
+        return {
+            count,
+            filter,
+            results: games,
+        };
+    }
+
+    public GetRoms = async (context: Tools.Context): Promise<any> => {
+
+        const facetPropertyNames = ['datafile', 'game'];
+
+        let roms: any[] = this.Cache.Tables['rom'];
+
+        // Filter
+        const { items, filter } = this.Filter(roms, facetPropertyNames, context.request.queryParameters);
+        roms = items;
+
+        // Slice
+        const count = roms.length;
+        const offset: number = context.request.queryParameters['offset'].slice(-1)[0];
+        const limit: number = context.request.queryParameters['limit'].slice(-1)[0];
+        roms = roms.slice(offset, offset + limit);
+
+        // Prepare
+        roms = roms.map((item: any) => ({...item}));
+        roms.forEach((rom: any) => {
+
+            rom.game_name = this.GameIdNameLookup[rom.game_Id];
+            rom.datafile_name = this.DatafileIdNameLookup[rom.datafile_Id];
+
+            delete rom.rom_Id;
+            delete rom.game_Id;
+            delete rom.datafile_Id;
+        });
+
+        return {
+            count,
+            filter,
+            results: roms,
+        };
+    }
 
 }
